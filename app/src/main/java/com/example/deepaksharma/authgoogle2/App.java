a024356f33408ae90efb75e7fced49206c27fa75
Deepak Sharma <deepaksharma040695@gmail.com>
2018-08-20 22:24:30 +0530

    commit cache of login maintain.

--- app/src/main/java/com/example/deepaksharma/authgoogle2/App.java
+++ app/src/main/java/com/example/deepaksharma/authgoogle2/App.java
@@ -3,8 +3,6 @@ package com.example.deepaksharma.authgoogle2;
 import android.app.Application;
 import com.example.sociallogin.SocialLogin;
 
-//import app.com.tasociallogin.network.TASocialLogin;
-
 public class App extends Application {
     @Override
     public void onCreate() {
